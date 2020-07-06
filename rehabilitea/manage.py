aa1e0ddbe628ad40a8b7bd1e9dd7325aa3ae3edd
Roberto Plaza Romero <36627781+RoberPlaza@users.noreply.github.com>
2020-07-06 17:41:59 +0200

    Added a few endpoints

--- rehabilitea/manage.py
+++ rehabilitea/manage.py
@@ -2,7 +2,7 @@
 
 from flask.cli import FlaskGroup
 
-from user_management import app, db, User
+from user_management import app, db, User, Difficulty, Game, Progression
 
 cli = FlaskGroup(app)
 
@@ -14,7 +14,24 @@ def create_db():
 
 @cli.command("seed_db")
 def create_db():
-    db.session.add(User())
+    db.session.add(User(id = 0, birth_day = "1996/08/01"))
+    db.session.add(User(id = 1, birth_day = "1999/04/12"))
+
+    db.session.add(Difficulty(id = 0, name = "Easy"))
+    db.session.add(Difficulty(id = 1, name = "Medium"))
+    db.session.add(Difficulty(id = 2, name = "Hard"))
+
+    db.session.add(Game(id = 0, name = "Memory"))
+    db.session.add(Game(id = 1, name = "Bubbles"))
+    db.session.add(Game(id = 2, name = "HideAndSeek"))
+
+    db.session.commit()
+
+    db.session.add(Progression(user = 0, game = 0, difficulty = 0))
+    db.session.add(Progression(user = 0, game = 0, difficulty = 1))
+    db.session.add(Progression(user = 0, game = 1, difficulty = 0))
+    db.session.add(Progression(user = 0, game = 2, difficulty = 0))
+
     db.session.commit()
 
 if __name__ == "__main__":
--- rehabilitea/user_management/__init__.py
+++ rehabilitea/user_management/__init__.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, render_template
-from flask_sqlalchemy import SQLAlchemy
+import datetime 
 
+from flask import Flask, jsonify, render_template, abort, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object("user_management.config.Config")
 
 db = SQLAlchemy(app)
+
 
 class User(db.Model):
     __tablename__   = "users"
@@ -25,7 +27,7 @@ class Game(db.Model):
     __tablename__   = "games"
 
     id              = db.Column(db.Integer, primary_key=True)
-    name            = db.Column(db.String(50), nullable=False)
+    name            = db.Column(db.String(50), nullable=False, unique=True)
 
 class Progression(db.Model):
     __tablename__   = "progressions"
@@ -34,6 +36,7 @@ class Progression(db.Model):
     user            = db.Column(db.Integer, db.ForeignKey('users.id'))
     game            = db.Column(db.Integer, db.ForeignKey('games.id'))
     difficulty      = db.Column(db.Integer, db.ForeignKey('difficulties.id'))
+    date_achieved   = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
 class Event(db.Model):
     __tablename__   = "events"
@@ -41,12 +44,40 @@ class Event(db.Model):
     id              = db.Column(db.Integer, primary_key=True)
     user            = db.Column(db.Integer, db.ForeignKey('users.id'))
     game            = db.Column(db.Integer, db.ForeignKey('games.id'))
+    time_produced   = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     event_type      = db.Column(db.String(50), nullable=False)
+
 
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template("index.html")
+
+@app.route("/difficulty/<int:id>/<string:game>", methods = ["GET", "POST"])
+@app.route("/difficulty/<string:game>/<int:id>", methods = ["GET", "POST"])
+def difficulty(id, game):
+    def get_difficulty(id, game):
+        game_obj    = Game.query.filter_by(name=game).first()
+        if not game_obj:
+            abort(404)
+
+        progression = Progression.query.filter_by(user=id, game=game_obj.id).order_by(Progression.date_achieved.desc()).first()
+        if not progression:
+            abort(404)
+            
+        difficulty  = Difficulty.query.filter_by(id = progression.difficulty).first()
+        if not difficulty:
+            abort(404)
+
+        return jsonify(difficulty=difficulty.name)
+    
+    def post_difficulty(id, game):
+        pass
+
+    if request.method == 'GET':
+        return get_difficulty(id, game)
+    elif request.method == 'POST':
+        return post_difficulty(id, game)
 
 if __name__ == "__main__":
     app.run()
